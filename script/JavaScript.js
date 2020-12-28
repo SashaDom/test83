0a122d3d2d03baa99e5017434e73c207788c77d6
SashaDom <a.domontovich@gmail.com>
2020-12-28 16:55:25 +0200

    28.12.2020

--- script/JavaScript.js
+++ script/JavaScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Scroll menu
 		$(document).ready(function(){
 			var wnd = $(window)
@@ -67,73 +66,3 @@
 			  $('.grids').isotope({ filter: filterValue });
 			});
 		});
-=======
-//Scroll menu
-		$(document).ready(function(){
-			var wnd = $(window)
-			var menu = $('.navbar')
-			var links = $('.nav-link')
-
-			console.log(links)
-
-			wnd.scroll(function (){
-				var top = wnd.scrollTop()
-				var bgColor = null
-				var socialItem=$('.first-social__item a')
-				var firstTime=$('.first__time')
-				// var firstTime=$('.first time')
-				var stylesScrol={ 
-					activeColor:'#EBC300',
-					lightColor:'#fff',
-					borderColor:'rgba(255,255,255,0.2)'
-				}
-				if (top > 100) {
-					bgColor = '#000'
-					links.css('padding', '10px 8px')
-					socialItem.css('borderColor',stylesScrol.activeColor)
-					socialItem.css('color',stylesScrol.activeColor)
-					firstTime.css('color',stylesScrol.activeColor)
-                   }
-				 else {
-					bgColor = 'transparent'
-					links.css('padding', '30px 8px')
-					socialItem.css('borderColor',stylesScrol.borderColor)
-					socialItem.css('color',stylesScrol.lightColor)
-                    firstTime.css('color',stylesScrol.lightColor)
-				}
-
-				menu.css('backgroundColor', bgColor)
-			})
-			// =================================================
-           // slider
-             $('.sliderSpec').slick({
-				arrows: true,
-				dots: true
-
-			});
-
-
-			$("#lightgallery").lightGallery();  
-// ============================================================// =================================================
-           // slider
-             $('.sliderSpec').slick({
-				arrows: true,
-				dots: true
-
-			});
-
-
-			$("#lightgallery1").lightGallery();  
-// ============================================================
-			$('.grids').isotope({
-			  // options 
-			  itemSelector: '.grids-item',
-			  layoutMode: 'masonry'
-			});
-
-			$('.filter-button-group').on( 'click', 'button', function() {
-			  var filterValue = $(this).attr('data-filter');
-			  $('.grids').isotope({ filter: filterValue });
-			});
-		});
->>>>>>> fc89c0f12d4d8fb5c763d8d7306821402eb688a1
